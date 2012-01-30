dc43292db072e2794a251fa259ff3a9344697d43
Frandroid <frandroid@gmail.com>
2012-01-29 22:11:14 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stationary::Application.config.secret_token = '2741aecc6cf631add0eb9d5807063a999168c3b56b73800924c4ffc59c836f79c461c9154bbe4fc6ce46e5263290a8065ab1316c657484369fa96032ebc28b95'
